ffed7e421ab9bcce534786327c88b34a8073021e
Ajay <ajayrnairg@gmail.com>
2025-12-19 00:20:22 +0530

    Added Home page files

---
+++ components/Socials.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiLinkedinLine,
+  RiGithubLine
+} from "react-icons/ri";
+
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>{" "}
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>{" "}
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>{" "}
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>{" "}
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+    </div>
+  );
+};
+
+export default Socials;
